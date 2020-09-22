542ac67f932851b2383855c0500b419d2461212b
Benjamin Barnard <barnardwben@yahoo.com>
2020-09-22 12:48:33 +0200

    Created array of menu objects

---
+++ src/data.js
@@ -0,0 +1,84 @@
+const menu = [
+  {
+    id: 1,
+    category: "Breakfast",
+    title: "Mel's Whole Wheat Eggs Benedict",
+    price: 12.99,
+    image:
+      "https://images.unsplash.com/photo-1513442542250-854d436a73f2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=858&q=80",
+    description:
+      "Whole wheat toast, with chopped brocolli, sliced avacado, and a poached egg topped with Mel's secret seasoning",
+  },
+  {
+    id: 2,
+    category: "Dinner",
+    title: "Melxicana Platter",
+    price: 18.99,
+    image:
+      "https://images.unsplash.com/photo-1586511925558-a4c6376fe65f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos vitae ad doloribus odio sunt.",
+  },
+  {
+    id: 3,
+    category: "Dessert",
+    title: "Mel's Mountain of Mayhem",
+    price: 4.99,
+    image:
+      "https://images.unsplash.com/photo-1551024506-0bccd828d307?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos vitae ad doloribus odio sunt.",
+  },
+  {
+    id: 4,
+    category: "Breakfast",
+    title: "Mama Mel's French Toast Supreme",
+    price: 9.99,
+    image:
+      "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
+    description:
+      "Thick slices of Toast, topped with blueberries, bananas, strawberries, powdered sugar, and syrup",
+  },
+  {
+    id: 5,
+    category: "Dinner",
+    title: "Bolognese A la Mel",
+    price: 15.99,
+    image:
+      "https://images.unsplash.com/photo-1505932049984-db368d92fa63?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos vitae ad doloribus odio sunt.",
+  },
+  {
+    id: 6,
+    category: "Dessert",
+    title: "Oreo Mel Crunch Shake",
+    price: 4.99,
+    image:
+      "https://images.unsplash.com/photo-1563805042-7684c019e1cb?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTl8fGRlc3NlcnR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos vitae ad doloribus odio sunt.",
+  },
+  {
+    id: 7,
+    category: "Breakfast",
+    title: "All American Melman",
+    price: 12.99,
+    image:
+      "https://images.unsplash.com/photo-1524182732116-a3ad2034503c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    description:
+      "Eggs, sausages, ham, tomatoes, toast, spinach, mushrooms and beans. You name it! We got it!",
+  },
+  {
+    id: 8,
+    category: "Dessert",
+    title: "Strawberry Mel Cake Shake",
+    price: 4.99,
+    image:
+      "https://images.unsplash.com/photo-1572974201645-64d84597e4b3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjR8fGRlc3NlcnR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos vitae ad doloribus odio sunt.",
+  },
+];
+
+export default menu;
